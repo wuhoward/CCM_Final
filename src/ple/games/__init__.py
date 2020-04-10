4927a561c014a66d844f5d3489948e2c94406523
Hao-Ning Wu <hnw244@cuda5.cims.nyu.edu>
2020-04-10 01:18:08 -0400

    Remove unnecessary directory

--- src/ple/games/__init__.py
+++
@@ -1,7 +0,0 @@
-from ple.games.originalgame import originalGame
-from ple.games.nosemantics import nosemantics
-from ple.games.nosimilarity import nosimilarity
-from ple.games.noobject import noobject
-from ple.games.noaffordance import noaffordance
-from ple.games.continualgame import continualgame
-
